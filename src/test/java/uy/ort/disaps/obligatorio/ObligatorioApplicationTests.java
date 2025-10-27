8d2e88919043ddda113efe64a0986494cf0cd8f7
MT332575@fi365.ort.edu.uy <MT332575@fi365.ort.edu.uy>
2025-10-27 13:56:52 -0300

    estructura base

---
+++ src/test/java/uy/ort/disaps/obligatorio/ObligatorioApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.ort.disaps.obligatorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligatorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
